663941a08fc94616fecd2c53739a8d4ff358a044
matteusmaiarg <matteusmaiarg@gmail.com>
2025-09-14 02:02:09 -0300

    ajeitando erro do deploy

--- desafio-pokemon/src/components/button/index.tsx
+++ desafio-pokemon/src/components/button/index.tsx
@@ -2,24 +2,30 @@
 
 import { useState } from "react"
 
-export  function Button () {
+export function Button() {
+  const [clique, setClicado] = useState<"grid" | "lista">("grid")
+  const [pesquisa, setPesquisa] = useState("")
 
-    const [clique, setClicado] = useState<"grid"| "lista">("grid")
+  const alternarVisual = () => {
+    setClicado(clique === "grid" ? "lista" : "grid")
+  }
 
-    return (
-
-        
-
-         <div className=" bg-gray-800 rounded-md p-6">
+  return (
+    <div className="bg-gray-800 rounded-md p-6">
       <input
         type="text"
         value={pesquisa}
         placeholder="Encontre o Pokemon"
         onChange={(e) => setPesquisa(e.target.value)}
-        className="border p-2 mb-4 w-full max-w-sm rounded bg-gray-500 mt-6 ml-5"
+        className="border p-2 mb-4 w-full max-w-sm rounded bg-gray-500 mt-6 ml-5 text-white"
       />
 
-      <button onClick={() => 
-        {setClicado}} className="text-white ">ALTERAR VISUAL</button>
-    )
+      <button
+        onClick={alternarVisual}
+        className="text-white bg-blue-500 p-2 rounded hover:bg-blue-600"
+      >
+        ALTERAR VISUAL
+      </button>
+    </div>
+  )
 }
